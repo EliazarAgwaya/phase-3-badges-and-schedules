3bea40ff2ad9ad95d8cdc588d49372b0a99ddd84
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-08-18 04:34:14 +0300

    commit tree 3109637b53e2c1bbba68e0571d4f292c6a18822a

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+def badge_maker name
+   return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator arr
+   arr.collect do |name| "Hello, my name is #{name}."
+   end
+end
+puts batch_badge_creator(["Teddy", "eliazar"])
